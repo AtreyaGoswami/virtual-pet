24654fa36747c8c217f7119963e3b036aab55691
AtreyaGoswami <72393490+AtreyaGoswami@users.noreply.github.com>
2020-12-06 20:49:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+var dogI,happyI,
+
+function preload()
+{
+  dogI = loadImage("dogimg.png");
+  happyI = loadImage("dogimg.png");
+  
+  
+
+}
+
+function setup() {
+  database = firebase.database();
+  createCanvas(500, 500);
+  
+  dog = createSprite(250,250,10,10);
+  dog.addImage(dogI);
+  dog.scale = 0.15;
+
+}
+
+
+function draw() {  
+  background("black")
+
+ 
+  if(keyWentDown(UP_ARROW)){
+    dog.addImage(happyI);
+
+  }
+
+  if(keyWentUp(UP_ARROW)){
+    dog.addImage(dogI);
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
